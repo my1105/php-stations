a0d1a45f5be8c0548823d5d06199740fb1d49cc8
mayu <ykmshiss@gmail.com>
2025-08-04 02:53:09 +0900

    continue

--- src/Station05/Question.php
+++ src/Station05/Question.php
@@ -6,5 +6,21 @@ class Question
 {
     public function main(): int
     {
+        $array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        $sum = 0;
+
+        foreach ($array as $value) {
+            if ($value >= 8) {
+                break;
+            }
+
+            if ($value % 4 > 2) {
+                continue;
+            }
+
+            $sum += $value;
+        }
+
+        return $sum;
     }
 }
