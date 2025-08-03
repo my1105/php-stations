02cb35007747324220ea1d8d83969065e6f01282
mayu <ykmshiss@gmail.com>
2025-08-04 02:58:10 +0900

    配列1

--- src/Station06/Question.php
+++ src/Station06/Question.php
@@ -6,5 +6,21 @@ class Question
 {
     public function main(): array
     {
+        $array = ['red', 'blue', 'yellow'];
+
+        $key = array_search('yellow', $array);
+        if ($key !== false) {
+            unset($array[$key]);
+        }
+
+        array_splice($array, 0, 0, ['white', 'black']);
+
+        $indexRed = array_search('red', $array);
+        $indexBlue = array_search('blue', $array);
+        if ($indexRed !== false && $indexBlue !== false && $indexBlue === $indexRed + 1) {
+            array_splice($array, $indexBlue, 0, ['green']);
+        }
+
+        return array_values($array);
     }
 }
