bbd5c53660fb2e41be65d45782c3520c35d5a78f
mayu <ykmshiss@gmail.com>
2025-08-04 02:47:50 +0900

    switch 文

--- src/Station03/Practice1.php
+++ src/Station03/Practice1.php
@@ -6,8 +6,20 @@ class Practice1
 {
     public function main(): void
     {
-        // ここにサンプルコードを記述
-    }
+        $a = 'bar';
+        switch ($a) {
+        case 'foo':
+            echo '$aは foo です' . PHP_EOL;
+            break;
+        case 'bar':
+        case 'baz':
+            echo '$aは bar または baz です' . PHP_EOL;
+            break;
+        default:
+            echo '$aは foo, bar, baz 以外です' . PHP_EOL;
+        }
+        
+    }   
 }
 
 (new Practice1())->main();
--- src/Station03/Question.php
+++ src/Station03/Question.php
@@ -6,5 +6,14 @@ class Question
 {
     public function main(mixed $arg): string
     {
+        if ($arg === 1 && is_int($arg)) {
+            return 'りんご';
+        }
+
+        if (($arg === 2 || $arg === 3) && is_int($arg)) {
+            return 'みかん';
+        }
+
+        return 'さくらんぼ';
     }
 }
