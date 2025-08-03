7540b7968e98ab75184183a158c177e4328bcf3b
mayu <ykmshiss@gmail.com>
2025-08-04 03:11:07 +0900

    配列1

--- src/Station07/Question.php
+++ src/Station07/Question.php
@@ -9,12 +9,23 @@ class Question
         $array1 = ['北海道' => 1, '東京都' => 13, '大阪府' => 'XX'];
         $array2 = ['広島県' => 34, '京都府' => 26];
         $array3 = ['京都府' => 'XX', '大阪府' => 27];
+
+        $merged = array_merge($array1, $array3, $array2);
+
+        return $merged;
     }
 
     public function two(): array
     {
         $firstNames = ['太郎', '次郎', '花子'];
         $lastNames = ['山田', '鈴木', '佐藤'];
+
+        $fullNames = [];
+        for ($i = 0; $i < count($firstNames); $i++) {
+            $fullNames[] = $lastNames[$i] . $firstNames[$i];
+        }
+
+        return $fullNames;
     }
 
     public function three(): array
@@ -24,5 +35,12 @@ class Question
             ['name' => 'banana', 'price' => 200],
             ['name' => 'orange', 'price' => 120],
         ];
+
+        $result = [];
+        foreach ($array as $item) {
+            $result[$item['name']] = $item['price'];
+        }
+
+        return $result;
     }
 }
