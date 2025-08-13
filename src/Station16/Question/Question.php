f85de32da1a78e2758f51c54148df8534df24b4e
mayu <ykmshiss@gmail.com>
2025-08-14 08:44:59 +0900

    変数や引数、返り値の「型」

--- src/Station16/Question/Question.php
+++ src/Station16/Question/Question.php
@@ -6,6 +6,11 @@ class Question
 {
     public function main(string $humanName, string $carName, int $passengers): void
     {
+        $human = new Human($humanName);
+        $car = new Car($carName);
+
+        echo $human->buyCar($car);
+        echo $car->run();
+        echo $car->pickup($passengers);
     }
 }
-
