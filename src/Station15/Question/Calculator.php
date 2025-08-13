47fe56fdabd33cf093793d8a2729c63a0b19e198
mayu <ykmshiss@gmail.com>
2025-08-14 08:33:18 +0900

    スコープ

--- src/Station15/Question/Calculator.php
+++ src/Station15/Question/Calculator.php
@@ -2,3 +2,15 @@
 
 namespace Src\Station15\Question;
 
+class Calculator
+{
+     public function multiply(array $multiplieds, int $multiplier)
+    {
+        if (empty($multiplieds)) {
+            return false;
+        }
+
+        return array_map(fn($n) => $n * $multiplier, $multiplieds);
+    }
+    
+}
